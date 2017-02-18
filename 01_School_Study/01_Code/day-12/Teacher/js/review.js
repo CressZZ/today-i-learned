810323bc0436055c0d845ac03640bc7a73155ac5
박영진 <dktsjssnrnsi@gmail.com>
2017-02-18 16:15:37 +0900

    update

---
+++ 01_School_Study/01_Code/day-12/Teacher/js/review.js
@@ -0,0 +1,24 @@
+/*! Review.js © yamoo9.net, 2017 */
+
+/**
+ * --------------------------------
+ * DOM API
+ * ================================
+ * DOM Properties
+ * Node Interface
+ * .firstChild : Element 뿐 아니라, 띄어쓰기 같은 text노드도 되지만, Value값은 접근 못함
+ * .lastChild
+ * .nextSibling
+ * .previousSibing
+
+ 
+ * .parentNode
+ * .childNodes
+ * .children
+
+
+ * .firstElementChild : Element 노드에 접근할 수 있다.
+ * .lastElementChild
+ * .nextElementSibling
+ * .previousElementSibing
+ * ----------------------------- */
