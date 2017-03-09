1fe52ef9bc2c848204f4b78bcd22f3b11992e743
박영진 <dktsjssnrnsi@gmail.com>
2017-03-09 12:05:44 +0900

    20일차수업내용

---
+++ 01_School_Study/01_Code/day-23_DAY-20/js/dom.js
@@ -0,0 +1,19 @@
+
+  function namespace(ns) {
+    return window[ns] = window[ns] || {};
+  }
+
+
+
+(function (global, $){
+
+})();
+
+/**
+ * [] 헬퍼 함수
+ * @author   cressZZ
+ * @version  1.0.0
+ * @param    {string} selector_str
+ * @param    {} context
+ * @return   {ElementNode} 문서 요소 노드 반환
+ */
